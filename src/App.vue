4f02d848bb28f32ac371d059a8bf8f9bcb8c5e67
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-30 01:07:32 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, 
+    ToDo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
